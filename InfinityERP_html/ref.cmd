728c5e3a9463b856b9db35271792fd20faab8dc1
Amr Farahat <amrfarahat83@gmail.com>
2025-05-22 10:43:15 +0300

    amr1

---
+++ InfinityERP_html/ref.cmd
@@ -0,0 +1,39 @@
+/* Skull Img */
+
+.alpha {
+   display: flex;
+   justify-content: flex-end;
+   align-items: center;
+   float: right;
+   }
+
+   /* bookmark */
+
+.bookmark_icon {
+    margin-left: 52px;
+}
+
+/* logo */
+
+.logo{
+    margin-left: 307px;
+    padding-top: 129px;
+}
+
+
+.sign_text h1{
+    color: var(--main-color-blue);
+    padding-top: 46px;
+}
+.sign_text p{
+    color: var(--secondry-clolor);
+    /* margin-left: 249px; */
+}
+.sign_text {
+    margin-left: 367px;
+    
+}
+
+.clean{
+    clear:both;
+ }
